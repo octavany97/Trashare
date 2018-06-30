3fbf6f3d61d06ad296a11f10e43a44f29cc73b40
Octavany <octavanyy97@gmail.com>
2018-06-30 16:59:31 +0700

    Udah bisa ambil tanggal di UpdateJadwalFramgent

--- app/src/main/java/id/ac/umn/trashare/UpdateJadwalFragment.java
+++ app/src/main/java/id/ac/umn/trashare/UpdateJadwalFragment.java
@@ -7,30 +7,32 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.TextView;
 
+import java.util.GregorianCalendar;
+
 public class UpdateJadwalFragment extends Fragment {
-
-
-
+    private String datePick;
+    TextView date;
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
-
         View v = inflater.inflate(R.layout.fragment_update_jadwal, container, false);
-
+        date = (TextView) v.findViewById(R.id.date);
 
         CalendarView mCalender = (CalendarView) v.findViewById(R.id.calenderview);
-//        mCalender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-//            @Override
-//            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-//                String date = dayOfMonth + "/" + month + "/" + year;
-//            }
-//        });
+        mCalender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int dayOfMonth) {
+                GregorianCalendar getDate = new GregorianCalendar(year, month, dayOfMonth);
+                datePick = dayOfMonth + "/" + month + "/" + year;
+                date.setText(datePick);
+            }
+        });
 
-//        TextView textView = (TextView) v.findViewById(R.id.date);
-//        textView.setText(date);
+        Button updateButton = (Button) v.findViewById(R.id.btn_update_tanggal);
 
         return v;
     }
