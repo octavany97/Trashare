c588f66a7fe75eba5d46ce4c2fed5dd1f23d2cad
Unknown <indrasaksana2000@gmail.com>
2018-07-05 05:06:52 +0700

    Add event kegiatan pengurus

--- app/src/main/java/id/ac/umn/trashare/AddEventPengurusActivity.java
+++ app/src/main/java/id/ac/umn/trashare/AddEventPengurusActivity.java
@@ -2,6 +2,16 @@ package id.ac.umn.trashare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import id.ac.umn.trashare.models.Kegiatan;
+import id.ac.umn.trashare.utils.Webservice;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AddEventPengurusActivity extends AppCompatActivity {
 
@@ -9,5 +19,31 @@ public class AddEventPengurusActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event_pengurus);
+
+        final EditText namaEvent = (EditText) findViewById(R.id.editNamaEvent);
+        final EditText descriptionEvent = (EditText) findViewById(R.id.editDescriptionEvent);
+        final EditText tempatEvent = (EditText) findViewById(R.id.editTempatEvent);
+        final EditText tanggalEvent = (EditText) findViewById(R.id.editTanggalEvent);
+        final EditText jamEvent = (EditText) findViewById(R.id.editJamEvent);
+
+        Button btnTambah = (Button) findViewById(R.id.btnAdd);
+        btnTambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Webservice.getService(getBaseContext()).createKegiatan(new Kegiatan(namaEvent.getText().toString(),descriptionEvent.getText().toString(),tempatEvent.getText().toString(),tanggalEvent.getText().toString() + " " + jamEvent.getText().toString())).enqueue(new Callback<Kegiatan>() {
+                    @Override
+                    public void onResponse(Call<Kegiatan> call, Response<Kegiatan> response) {
+                        Toast.makeText(AddEventPengurusActivity.this, "Kegiatan berhasil dibuat", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(Call<Kegiatan> call, Throwable t) {
+                        Toast.makeText(AddEventPengurusActivity.this, "Kegiatan gagal dibuat", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/id/ac/umn/trashare/models/Kegiatan.java
+++ app/src/main/java/id/ac/umn/trashare/models/Kegiatan.java
@@ -8,4 +8,10 @@ public class Kegiatan implements Serializable {
     public String namaKegiatan;
     public String tanggalKegiatan;
     public String fotoKegiatan;
+
+    public Kegiatan(String namaKegiatan,String deskripsiKegiatan,String tempatEvent,String tanggalKegiatan){
+        this.namaKegiatan = namaKegiatan;
+        this.deskripsiKegiatan = deskripsiKegiatan;
+        this.tanggalKegiatan = tanggalKegiatan;
+    }
 }
