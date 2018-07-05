4796604b1977d4f779cf60bde29642c20a2be7e4
Unknown <vionilyn27@gmail.com>
2018-07-05 08:53:39 +0700

    Maps

--- app/src/main/java/id/ac/umn/trashare/FormRegistrasiBankSampahActivity.java
+++ app/src/main/java/id/ac/umn/trashare/FormRegistrasiBankSampahActivity.java
@@ -1,19 +1,32 @@
 package id.ac.umn.trashare;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class FormRegistrasiBankSampahActivity extends AppCompatActivity {
+
+    EditText txtAlamat;
+    EditText txtNamaBankSampah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_registrasi_bank_sampah);
 
+        txtNamaBankSampah = (EditText) findViewById(R.id.nama_banksampah_edit);
+        txtAlamat = (EditText) findViewById(R.id.alamat_edit);
+        txtAlamat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(FormRegistrasiBankSampahActivity.this, MapActivity.class), 0);
+            }
+        });
         String [] values =
                 {"RT","RW","KECAMATAN","KELURAHAN"};
 
@@ -37,5 +50,18 @@ public class FormRegistrasiBankSampahActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+            Bundle extras = data.getExtras();
+            if (extras != null) {
+                String address = extras.getString("ADDRESS");
+                txtAlamat.setText(address);
+            }
+
+        }
     }
 }
